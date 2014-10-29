e68f316c17312c8900943f5a128d5cff1003f112
steveo1485 <stevennugent@gmail.com>
2014-10-28 22:27:25 -0700

    Update oauth token on facebook callback if empty or about to expire.

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -4,6 +4,11 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
+      if @user.oauth_token.blank? or @user.oauth_token_expires_at <= (Time.now - 1.week)
+        @user.oauth_token = request.env["omniauth.auth"].credentials.token
+        @user.oauth_token_expires_at = request.env["omniauth.auth"].credentials.expires_at
+        @user.save(validate: false)
+      end
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,8 @@ class User < ActiveRecord::Base
       user.password = Devise.friendly_token[0,20]
       user.first_name = auth.info.first_name
       user.last_name = auth.info.last_name
+      user.oauth_token = auth.credentials.token
+      user.oath_token_expires_at = auth.credentials.expires_at
     end
   end
 
