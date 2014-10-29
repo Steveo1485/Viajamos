51ba93df546048b6daca88a2eff418c71ac87f06
steveo1485 <stevennugent@gmail.com>
2014-10-28 22:29:31 -0700

    Add temp facebook data to planner for heroku preview.

--- app/controllers/planner_controller.rb
+++ app/controllers/planner_controller.rb
@@ -3,6 +3,9 @@ class PlannerController < ApplicationController
 
   def index
     gon.facebook_app_id = ENV['FACEBOOK_APP_ID']
+    ##### TEMP FACEBOOK CHECK
+    graph = Koala::Facebook::API.new(current_user.oauth_token)
+    @temp_fb_check = graph.get_connections("me", "friends")
   end
 
 end
