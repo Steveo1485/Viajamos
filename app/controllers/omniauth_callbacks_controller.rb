1343c6c8ad8680babe02f053347fc927f2b4208b
steveo1485 <stevennugent@gmail.com>
2014-10-29 08:06:47 -0700

    Fix error with fb oauth.

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -4,7 +4,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      if @user.oauth_token.blank? or @user.oauth_token_expires_at <= (Time.now - 1.week)
+      if @user.oauth_token.blank? or Time.at(@user.oauth_token_expires_at.to_i) <= (Time.now - 1.week)
         @user.oauth_token = request.env["omniauth.auth"].credentials.token
         @user.oauth_token_expires_at = request.env["omniauth.auth"].credentials.expires_at
         @user.save(validate: false)
--- app/controllers/planner_controller.rb
+++ app/controllers/planner_controller.rb
@@ -4,8 +4,10 @@ class PlannerController < ApplicationController
   def index
     gon.facebook_app_id = ENV['FACEBOOK_APP_ID']
     ##### TEMP FACEBOOK CHECK
-    graph = Koala::Facebook::API.new(current_user.oauth_token)
-    @temp_fb_check = graph.get_connections("me", "friends")
+    if current_user.oauth_token
+      graph = Koala::Facebook::API.new(current_user.oauth_token)
+      @temp_fb_check = graph.get_connections("me", "friends")
+    end
   end
 
 end
