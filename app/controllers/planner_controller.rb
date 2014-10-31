127e4f2492fe55105bd76391cfa315a9da4c3347
steveo1485 <stevennugent@gmail.com>
2014-10-30 20:50:25 -0700

    Request user_friends with omniauth.

--- app/controllers/planner_controller.rb
+++ app/controllers/planner_controller.rb
@@ -7,6 +7,7 @@ class PlannerController < ApplicationController
     if current_user.oauth_token
       graph = Koala::Facebook::API.new(current_user.oauth_token)
       @temp_fb_check = graph.get_connections("me", "friends")
+      @temp_fb_check2 = graph.get_connections("me", "friends", api_version: "v2.0")
     end
   end
 
