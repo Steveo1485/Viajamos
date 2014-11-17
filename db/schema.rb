e6f2b3076907b2df21c6121855df5e30691f6f5d
Steven Nugent <stevennugent@gmail.com>
2014-11-16 16:42:19 -0800

    Start trip ids at 1000

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141117000157) do
+ActiveRecord::Schema.define(version: 20141117004140) do
 
   create_table "delayed_jobs", force: true do |t|
     t.integer  "priority",   default: 0, null: false
