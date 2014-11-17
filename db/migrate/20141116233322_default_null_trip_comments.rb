8c834fc91b1384e17a319480b40ad2779d07b5bf
Steven Nugent <stevennugent@gmail.com>
2014-11-16 16:38:03 -0800

    Remove changing existing comments from migration.

--- db/migrate/20141116233322_default_null_trip_comments.rb
+++ db/migrate/20141116233322_default_null_trip_comments.rb
@@ -1,11 +1,5 @@
 class DefaultNullTripComments < ActiveRecord::Migration
   def change
-    Trip.all.each do |trip|
-      unless trip.comments.present?
-        trip.comments = nil
-        trip.save
-      end
-    end
     change_column_default :trips, :comments, nil
   end
 end
