a57594c00412a6132380b874e69f3d77569344c6
Steven Nugent <stevennugent@gmail.com>
2015-06-24 07:35:57 -0700

    Checkpoint - homepage cleanup

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,4 @@
 class PagesController < ApplicationController
-  layout "pages"
-
   skip_before_action :authenticate_user!
   skip_after_action :verify_authorized
   skip_after_action :verify_policy_scoped
